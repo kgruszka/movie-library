58308011d4c4db144ed95fa50d3399aa618507a1
pearman_autea <kamil.gruszka@autea.pl>
2017-05-30 23:50:25 +0200

    movies fixture

---
+++ src/web/acceptance/v1/fixtures/index.js
@@ -0,0 +1,6 @@
+'use strict'
+const movies = require('./movies')
+
+module.exports = {
+  movies
+}
---
+++ src/web/acceptance/v1/fixtures/movies.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const movies = [
+  {
+    title: 'Title1',
+    rating: 4.23,
+    category: 'Comedy'
+  },
+  {
+    title: 'Title2',
+    rating: 2.42,
+    category: 'Thriller'
+  }
+]
+
+module.exports = movies
