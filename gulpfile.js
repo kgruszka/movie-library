d7f09f4217c1e60e00fae60344e9659a89f0f555
pearman_autea <kamil.gruszka@autea.pl>
2017-05-31 23:11:44 +0200

    gulp tasks: full environment with network and mongo

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,10 @@ const DEPENDENCIES_IMAGE = 'movie-library-dep'
 const DEPENDENCIES_CONTAINER = 'movie-library-dep'
 const DEVELOPMENT_IMAGE_FILE = 'Dockerfile.dev'
 const DEPENDENCIES_IMAGE_FILE = 'Dockerfile.dev.dep'
+const DEVELOPMENT_NETWORK = 'movie-library-network'
+const DEVELOPMENT_NETWORK_SUBNET = '172.25.10.0/24'
+const DEVELOPMENT_MONGO_CONTAINER = 'movie-library-mongo'
+const MONGO_IMAGE = 'mongo:3.2'
 
 async function cmd (command, options = {}) {
   return new Promise((resolve, reject) => {
@@ -29,12 +33,38 @@ gulp.task('dev:build', () => {
   return Promise.all([buildDependenciesImage, buildDevelopmentImage])
 })
 
+
+gulp.task('dev:network:create', () => {
+  return cmd(`docker network create -d bridge --subnet ${DEVELOPMENT_NETWORK_SUBNET} ${DEVELOPMENT_NETWORK}`)
+})
+
+gulp.task('dev:network:rm', async () => {
+  const exists = await cmd(`docker network ls -q -f name=${DEVELOPMENT_NETWORK}`)
+  if (exists) {
+    return cmd(`docker network rm ${DEVELOPMENT_NETWORK}`)
+  }
+})
+
+gulp.task('dev:mongo:run', () => {
+  return cmd(`docker run -d --name ${DEVELOPMENT_MONGO_CONTAINER} --network ${DEVELOPMENT_NETWORK} ${MONGO_IMAGE}`)
+})
+
+gulp.task('dev:mongo:rm', async () => {
+  const exists = await cmd(`docker ps -a -q -f name=${DEVELOPMENT_MONGO_CONTAINER}`)
+  if (exists) {
+    return cmd(`docker rm -f ${DEVELOPMENT_MONGO_CONTAINER}`)
+  }
+})
+
 gulp.task('dev:run', () => {
-  return cmd(`docker run -d --rm -p 3000:3000 -v %cd%:/opt/project --volumes-from ${DEPENDENCIES_CONTAINER} --name ${DEVELOPMENT_CONTAINER} ${DEVELOPMENT_IMAGE}`)
+  return cmd(
+    `docker run -d --rm -p 3000:3000 -v %cd%:/opt/project --volumes-from ${DEPENDENCIES_CONTAINER}`
+     + ` --name ${DEVELOPMENT_CONTAINER} --network ${DEVELOPMENT_NETWORK} ${DEVELOPMENT_IMAGE}`
+  )
 })
 
 gulp.task('dev:rm', async () => {
-  const exists = await cmd(`docker ps -q -f name=${DEVELOPMENT_CONTAINER}`)
+  const exists = await cmd(`docker ps -a -q -f name=${DEVELOPMENT_CONTAINER}`)
   if (exists) {
     return cmd(`docker rm -v -f ${DEVELOPMENT_CONTAINER}`)
   }
@@ -51,13 +81,19 @@ gulp.task('dev:dep:rm', async () => {
   }
 })
 
-gulp.task('dev:clean', ['dev:rm', 'dev:dep:rm'])
+gulp.task('dev:clean', cb => {
+  seq(
+    ['dev:rm', 'dev:dep:rm', 'dev:mongo:rm'],
+    'dev:network:rm',
+    cb
+  )
+})
 
 gulp.task('dev', cb => {
   seq(
-    ['dev:build', 'dev:clean'],
-    'dev:dep:create',
-    'dev:run',
+    ['dev:clean'],
+    ['dev:dep:create', 'dev:network:create'],
+    ['dev:mongo:run', 'dev:run'],
     cb
   )
 })
