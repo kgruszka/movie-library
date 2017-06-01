ca2a96ec1497ae04952073caf1a901da01a80f1c
pearman_autea <kamil.gruszka@autea.pl>
2017-06-02 00:50:31 +0200

    test util module refactor

--- src/utils/test.js
+++ src/utils/test.js
@@ -5,7 +5,7 @@ function isRouteRegistered (router, {path, method}) {
   if (typeof path !== 'string') throw new InvalidArgumentError({path})
   if (typeof method !== 'string') throw new InvalidArgumentError({method})
   const routes = router.stack
-      .filter(r => r.route.path === path && r.route.methods[method])
+      .filter(({route}) => route.path === path && route.methods[method])
   return routes.length === 1
 }
 
