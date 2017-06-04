27d5b1d7f625888aeea5b685b440e7b49c6f3d8c
pearman_autea <kamil.gruszka@autea.pl>
2017-06-04 17:20:26 +0200

    mongodb config - db name handling

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,15 +3,18 @@
 const configs = {
   test: {
     host: process.env.MONGO_HOST || 'localhost',
-    port: process.env.MONGO_PORT || 27017
+    port: process.env.MONGO_PORT || 27017,
+    dbName: 'test'
   },
   development: {
     host: process.env.MONGO_HOST || 'localhost',
-    port: process.env.MONGO_PORT || 27017
+    port: process.env.MONGO_PORT || 27017,
+    dbName: process.env.MONGO_DB_NAME || 'movie-library'
   },
   production: {
     host: process.env.MONGO_HOST,
-    port: process.env.MONGO_PORT
+    port: process.env.MONGO_PORT,
+    dbName: process.env.MONGO_DB_NAME
   }
 }
 
@@ -24,8 +27,9 @@ const config = configs[env]
 if (!config.host || !config.port) throw Error('MongoDB configuration error. Host/Port not specified.')
 
 module.exports = {
-  getUrl: (dbName) => {
+  getUrl: (dbName = '') => {
     if (typeof dbName !== 'string') throw Error('Mongodb configuration error. Database name must be a string.')
+    if (!dbName) dbName = config.dbName
     return `mongodb://${config.host}:${config.port}/${dbName}`
   }
 }
